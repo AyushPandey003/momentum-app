b8ba4255041d3ceef1a98b79797e8c4b20204a20
Ayush Pandey <ayush12122003@gmail.com>
2025-11-17 17:17:50 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/backend/:path*",
+        source: "/backend/:path*",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/backend/:path*"
-            : "/api/backend/",
+            : "/api/backend/index.py",
       },
     ];
   },
